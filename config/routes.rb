372b0b15637a96b4e8fa3302212c3d3eacd740f9
Dariusch Ochlast <johndoe@example.com>
2013-11-09 15:47:47 +0100

    Comments zu Posts hinzugefügt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Basisapp::Application.routes.draw do
   devise_for :users
   resources :posts
+  resources :comments
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
