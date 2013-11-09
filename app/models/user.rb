75808498023cc7e323afdd6243fe0b716bb47240
Höltermann <holtermann@josephines-MacBook-Pro.local>
2013-11-09 15:45:58 +0100

    add username and date to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :user_name, :presence => true, uniqueness: true
+
   has_many :posts
 end
